22cc4103df06b2e747b2a3cc85826f8f540b375b
Neil MacGregor <nmacgreg@ualberta.ca>
2022-11-23 16:22:47 -0700

    Printing all lines, gotta do some cleaning

--- populate.py
+++ populate.py
@@ -6,7 +6,18 @@ with open('metadata2.csv', mode ='r')as file:
   #metadata = csv.reader(file)
   metadata = csv.DictReader(file)
   for line in metadata:
-        print(line[])
+        print(f'<!--{line["Display title"]}-->')
+        print(f'<div class="card card-body item {line["Coverage-Province"]} {line["Language"]}">')
+        print(f'\t<h5><a href="https://archive.org/details/{line["Q code"]}">{line["Display title"]}</a></h5>')
+        print(f'\t<p class="ml-2 mb-0">{line["Pub start"]} to {line["Pub end"]}</p> ')
+        print('\t<hr/>')
+        print('<div class="small px-2 py-1">')
+        print(f'\t\t<p>{line["Notes"]}</p>')
+        print(f'<p>{line["Related items"]}</p>')
+        print('\t</div>')
+        print('</div>')
+
+        print()
 
 
 #print(f"First line: {metadata[0]}")
