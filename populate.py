b0b7eaeee095f540f13f09824a96f8259f6676d0
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2022-12-02 14:58:08 -0700

    First code cleanup

--- populate.py
+++ populate.py
@@ -1,21 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
+import sys
 import csv
 
-with open('metadata2.csv', mode ='r')as file:
-  #metadata = csv.reader(file)
+# Hardcoding file argument from sys.argv[1]
+
+if len(sys.argv) < 2:
+  print('Wrong number of arguments. You need to provide a filename')
+  sys.exit() 
+  
+# Hardcoding file argument from sys.argv[1]
+with open(sys.argv[1], mode ='r') as file:
   metadata = csv.DictReader(file)
   for line in metadata:
-        print(f'<!--{line["Display title"]}-->')
-        print(f'<div class="card card-body item {line["Coverage-Province"]} {line["Language"]}">')
-        print(f'\t<h5><a href="https://archive.org/details/{line["Q code"]}">{line["Display title"]}</a></h5>')
-        print(f'\t<p class="ml-2 mb-0">{line["Pub start"]} to {line["Pub end"]}</p> ')
-        print('\t<hr/>')
-        print('<div class="small px-2 py-1">')
-        print(f'\t<p>{line["Notes"]}</p>')
-        print(f'<p>{line["Related items"]}</p>')
-        print('\t</div>')
-        print('</div>')
-
-        print()
-
+    print(f'''
+      <!--{line["Display title"]}-->
+      <div class="card card-body item {line["Coverage-Province"]} {line["Language"]}">
+        <h5><a href="https://archive.org/details/{line["Q code"]}">{line["Display title"]}</a></h5>
+        <p class="ml-2 mb-0">{line["Pub start"]} to {line["Pub end"]}</p>
+        <hr/>
+        <div class="small px-2 py-1">
+        <p>{line["Notes"]}</p>
+        <p>{line["Related items"]}</p>
+        </div>
+      </div>
+    ''')
